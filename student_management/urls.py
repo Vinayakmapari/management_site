1a6c26aa3e90d96e31465ffc638a773cab174f8f
Vinayak B Mapari <95145886+Vinayakmapari@users.noreply.github.com>
2023-02-27 12:59:33 +0530

    setting_update

--- student_management/urls.py
+++ student_management/urls.py
@@ -51,7 +51,6 @@ urlpatterns = [
     path('Hod/Course/Delete/<str:id>',Hod_Views.DELETE_COURSE,name='delete_course'),
 
 
-
-   
-
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
